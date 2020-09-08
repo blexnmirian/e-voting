6512ab2212d5a27e9552d53c663cd86d332d3b02
blexnmirian <blexnmirian@gmail.com>
2020-09-08 14:37:47 +0100

    commit message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 
 const app = express();
@@ -12,10 +12,11 @@ const poll = require('./router/poll')
 app.use(express.static(path.join(__dirname, 'public')));
 
 //body parser middleware
+dotenv.config({path: './.env'})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-dotenv.config({path: './config.env'})
+
 //connecting to mongodb
 mongoose.connect(process.env.DATABASE_REMOTE, {
   useNewUrlParser: true,
